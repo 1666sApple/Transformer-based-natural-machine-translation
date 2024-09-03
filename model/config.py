f719a40058869b62dc7a6eee8e36ec1754559671
1666sApple <ananno.034@gmail.com>
2024-09-04 01:36:43 +0600

    Add config.py

---
+++ model/config.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+def getConfig():
+    return {
+        "batchSize": 8,
+        "numEpochs": 20,
+        "lr": 10**-4,
+        "seqLen": 256,
+        "dimModel": 512,
+        "srcData": 'opus_books',
+        "srcLang": "en",
+        "targetLang": "bn",
+        "modelFolder":"weigths",
+        "modelBaseName":"transformer",
+        "preload": None,
+        "tokenizerFile": "tokenizer{0}.json",
+        "experimentName": "runs/transformer"
+    }
+
+def getWeightFilePath(config, epoch: str):
+    modelFolder = f"{config['srcData']}_{config['modelFolder']}"
+    modelFileName = f"{config['modelBaseName']}{epoch}.pt"
+    return str(Path('.') / modelFolder / modelFileName)
+
+def lastWeightFilePath(config):
+    modelFolder = f"{config['srcData']}_{config['modelFolder']}"
+    modelFileName = f"{config['modelBaseName']}*"
+    weightFiles = list(Path(modelFolder).glob(modelFileName))
+    if len(weightFiles) == 0:
+        return None
+
+    weightFiles.sort()
+    return str(weightFiles[-1])
